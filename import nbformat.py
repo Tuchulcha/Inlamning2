16a3f395644f9bb610e28c16a684a776864b0f41
Tuchulcha <tonikorkia@gmail.com>
2024-02-01 19:46:38 +0100

    Tried tensorboard

--- import nbformat.py
+++ import nbformat.py
@@ -21,4 +21,21 @@ def copy_notebook_cells(file_path):
     print("Content copied to clipboard.")
 
 # Replace 'your_notebook.ipynb' with the path to your notebook
-copy_notebook_cells('Yolomodelling.ipynb')
+# copy_notebook_cells('C:\BIA\Kurs 8\Inlämning2\Experiment5\Exp5.ipynb')
+
+def copy_notebook_code_cells(file_path):
+    # Load the Jupyter Notebook
+    notebook = nbformat.read(file_path, as_version=4)
+
+    # Extract the content of code cells only
+    code_cell_contents = [cell['source'] for cell in notebook.cells if cell['cell_type'] == 'code']
+
+    # Join all code cell contents into a single string
+    all_code_content = '\n\n'.join(code_cell_contents)
+
+    # Copy to clipboard (you can also print or save to a file)
+    pyperclip.copy(all_code_content)
+    print("Code content copied to clipboard.")
+
+# Replace 'your_notebook.ipynb' with the path to your notebook
+copy_notebook_code_cells('C:\\BIA\\Kurs 8\\Inlämning2\\Experiment5\\Exp5.ipynb')
